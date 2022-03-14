9e28ccaede7507a2941a366bf8c40cd94f368e57
SiamSarker <shoib@cse.uiu.ac.bd>
2022-03-14 15:48:04 +0600

    Cloned the code in Main

---
+++ appt.php
@@ -0,0 +1,17 @@
+<?php
+	$name = $_GET["name"];
+	$date = $_GET["date"];
+	$from = $_GET["from"];
+	$to = $_GET["to"];
+	$status = $_GET["status"];
+
+	require_once('dbconfig.php');
+	$connect = mysqli_connect( HOST, USER, PASS, DB )
+		or die("Can not connect");
+
+	mysqli_query( $connect, "INSERT INTO ticket_booking VALUES ( '', '$name', '$date', '$from','$to','$status' )" )
+		or die("Can not execute query");
+
+
+	echo "<p><a href=index.php>READ all records</a>";
+?>
---
+++ appt_input.php
@@ -0,0 +1,15 @@
+<h1>Create Record</h1>
+
+<form method=get action=appt.php>
+	name: <input type=text name=name> <br>
+	<p>
+	date: <input type=text name=date> <br>
+	<p>
+		from: <input type=text name=from> <br>
+	<p>
+		to: <input type=text name=to> <br>
+	<p>
+		<input type=hidden name=status value="Null"> <br>
+	<p>
+	<input type=submit value=Insert>
+</form>
